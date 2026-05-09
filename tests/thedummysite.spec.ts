9793678ca9e4767899de159b07eb97bb6d6c6004
Suraj Saini <s2008saini@gmail.com>
2026-05-09 13:00:32 -0700

    first

--- tests/thedummysite.spec.ts
+++ tests/thedummysite.spec.ts
@@ -5,5 +5,5 @@ import { test, expect } from '@playwright/test';
 test('testD1', async ({ page }) => {
     await page.goto('https://thedummysite.com/panel');
 
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(5000);
 });
