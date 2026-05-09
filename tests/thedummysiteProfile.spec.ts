9180cb655c5d5da45dde3aeab4172bdd7623e488
Suraj Saini <s2008saini@gmail.com>
2026-05-09 14:41:49 -0700

    action-succ

--- tests/thedummysiteProfile.spec.ts
+++ tests/thedummysiteProfile.spec.ts
@@ -7,6 +7,7 @@ import { profilePage } from '../pages/profilePage';
 test('testD1', async ({ page }) => {
     const proPage = new profilePage(page);
     proPage.goto();
+    await expect(page.locator('#firstName')).toBeVisible();
     await expect(proPage.firstName).toHaveValue('MrDemo');
     await expect(page.locator('#firstName')).toBeDisabled();
 
